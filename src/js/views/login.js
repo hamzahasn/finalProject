d99df6291d044835413963719720237fe8502e58
rlacs4g <lacson.rafael@gmail.com>
2021-02-08 22:19:03 +0000

    Adding signup and signin pages. Changes to layout

---
+++ src/js/views/login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.scss";
+
+export const Login = () => (
+	<div className="text-center mt-5">
+		<form>
+			<div className="form-group">
+				<label>Email address</label>
+				<input
+					type="email"
+					className="form-control"
+					id="exampleInputEmail1"
+					aria-describedby="emailHelp"
+					placeholder="Enter email"
+				/>
+				<small id="emailHelp" className="form-text text-muted" />
+			</div>
+			<div className="form-group">
+				<label>Password</label>
+				<input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+			</div>
+			<button type="submit" className="btn btn-primary">
+				Submit
+			</button>
+		</form>
+	</div>
+);
