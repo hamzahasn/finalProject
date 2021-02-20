a37a1fa539ade2682226054a52f442ef6e4ee19e
hamzahasn <hamza.hasnain@gmail.com>
2021-02-20 04:23:39 +0000

    updated diary totals

--- src/js/views/diary.js
+++ src/js/views/diary.js
@@ -27,134 +27,173 @@ export const Diary = () => {
 	// );
 
 	return (
-		<>
-			<Container>
-				<div className="d-flex flex-row">
-					<h4 className="mr-2">Your Food Diary for:</h4>
-					<div>
-						<DatePicker className="calendar" onChange={setDate} value={date} />
-					</div>
+		<Container>
+			<div className="d-flex flex-row">
+				<h4 className="mr-2">Your Food Diary for:</h4>
+				<div>
+					<DatePicker className="calendar" onChange={setDate} value={date} />
 				</div>
+			</div>
 
-				<ListGroup className="diary-list-group">
-					<h2>Morning</h2>
-					<ListGroup.Item>
+			<ListGroup className="diary-list-group">
+				<h2>Morning</h2>
+				<ListGroup.Item>
+					<Row>
+						<Col>Name</Col>
+						<Col xs={1}>Qty</Col>
+						<Col xs={1}>Serving Size</Col>
+						<Col xs={1}>Unit</Col>
+						<Col xs={1}>Calories</Col>
+						<Col xs={1}>Action</Col>
+					</Row>
+				</ListGroup.Item>
+				{// find the diary for the selected date
+				// check if length > 0 for this part of the day
+				// if length > 0, map the array and display info
+
+				store.foodselected.morning.map((item, index) => (
+					<ListGroup.Item key={index}>
 						<Row>
-							<Col>Name</Col>
-							<Col xs={1}>Qty</Col>
-							<Col xs={1}>Serving Size</Col>
-							<Col xs={1}>Unit</Col>
-							<Col xs={1}>Calories</Col>
-							<Col xs={1}>Action</Col>
-						</Row>
-					</ListGroup.Item>
-					{// find the diary for the selected date
-					// check if length > 0 for this part of the day
-					// if length > 0, map the array and display info
+							<Col>{item.food_name}</Col>
+							<Col xs={1}>
+								<input
+									type="number"
+									min="1"
+									step="1"
+									value={item.qty}
+									onChange={e => actions.updateFoodQty(e.target.value, index, "morning")}
+									className="form-control"
+								/>
+							</Col>
+							<Col xs={1}>{item.serving_qty}</Col>
+							<Col xs={1}>{item.serving_unit}</Col>
+							<Col xs={1}>{item.nf_calories}</Col>
 
-					store.foodselected.morning.map((item, index) => (
-						<ListGroup.Item key={index}>
-							<Row>
-								<Col>{item.food_name}</Col>
-								<Col xs={1}>
-									<input
-										type="number"
-										min="1"
-										step="1"
-										value={item.qty}
-										onChange={e => actions.updateFoodQty(e.target.value, index, "morning")}
-										className="form-control"
-									/>
-								</Col>
-								<Col xs={1}>{item.serving_qty}</Col>
-								<Col xs={1}>{item.serving_unit}</Col>
-								<Col xs={1}>{item.nf_calories}</Col>
-
-								<Col xs={1}>
-									<Button onClick={e => actions.removeFoodItem(index, "morning")}>
-										<i className="fas fa-minus" />
-									</Button>
-								</Col>
-							</Row>
-						</ListGroup.Item>
-					))}
-					<ListGroup.Item>
-						<Diarysearch dailyFood="morning" />
-					</ListGroup.Item>
-				</ListGroup>
-				<ListGroup className="diary-list-group">
-					<h2>Afternoon</h2>
-					<ListGroup.Item>
-						<Row>
-							<Col>Name</Col>
-							<Col xs={1}>Qty</Col>
-							<Col xs={1}>Serving Size</Col>
-							<Col xs={1}>Unit</Col>
-							<Col xs={1}>Calories</Col>
-							<Col xs={1}>Action</Col>
-						</Row>
-					</ListGroup.Item>
-					{// find the diary for the selected date
-					// check if length > 0 for this part of the day
-					// if length > 0, map the array and display info
-
-					store.foodselected.afternoon.map((item, index) => (
-						<ListGroup.Item key={index}>
-							<Row>
-								<Col>{item.food_name}</Col>
-								<Col xs={1}>{item.qty}</Col>
-								<Col xs={1}>{item.serving_qty}</Col>
-								<Col xs={1}>{item.serving_unit}</Col>
-								<Col xs={1}>{item.nf_calories}</Col>
-
-								<Col xs={1}>
+							<Col xs={1}>
+								<Button onClick={e => actions.removeFoodItem(index, "morning")}>
 									<i className="fas fa-minus" />
-								</Col>
-							</Row>
-						</ListGroup.Item>
-					))}
-					<ListGroup.Item>
-						<Diarysearch dailyFood="afternoon" />
-					</ListGroup.Item>
-				</ListGroup>
-				<ListGroup className="diary-list-group">
-					<h2>Night</h2>
-					<ListGroup.Item>
-						<Row>
-							<Col>Name</Col>
-							<Col xs={1}>Qty</Col>
-							<Col xs={1}>Serving Size</Col>
-							<Col xs={1}>Unit</Col>
-							<Col xs={1}>Calories</Col>
-							<Col xs={1}>Action</Col>
+								</Button>
+							</Col>
 						</Row>
 					</ListGroup.Item>
-					{// find the diary for the selected date
-					// check if length > 0 for this part of the day
-					// if length > 0, map the array and display info
+				))}
+				<ListGroup.Item>
+					<Diarysearch dailyFood="morning" />
+				</ListGroup.Item>
+			</ListGroup>
 
-					store.foodselected.night.map((item, index) => (
-						<ListGroup.Item key={index}>
-							<Row>
-								<Col>{item.food_name}</Col>
-								<Col xs={1}>
-									<input type="number" min="1" step="1" value={item.qty} />
-								</Col>
-								<Col xs={1}>{item.serving_qty}</Col>
-								<Col xs={1}>{item.serving_unit}</Col>
-								<Col xs={1}>{item.nf_calories}</Col>
+			<ListGroup className="diary-list-group">
+				<h2>Afternoon</h2>
+				<ListGroup.Item>
+					<Row>
+						<Col>Name</Col>
+						<Col xs={1}>Qty</Col>
+						<Col xs={1}>Serving Size</Col>
+						<Col xs={1}>Unit</Col>
+						<Col xs={1}>Calories</Col>
+						<Col xs={1}>Action</Col>
+					</Row>
+				</ListGroup.Item>
+				{// find the diary for the selected date
+				// check if length > 0 for this part of the day
+				// if length > 0, map the array and display info
 
-								<Col xs={1}>
+				store.foodselected.afternoon.map((item, index) => (
+					<ListGroup.Item key={index}>
+						<Row>
+							<Col>{item.food_name}</Col>
+							<Col xs={1}>
+								<input
+									type="number"
+									min="1"
+									step="1"
+									value={item.qty}
+									onChange={e => actions.updateFoodQty(e.target.value, index, "afternoon")}
+									className="form-control"
+								/>
+							</Col>
+							<Col xs={1}>{item.serving_qty}</Col>
+							<Col xs={1}>{item.serving_unit}</Col>
+							<Col xs={1}>{item.nf_calories}</Col>
+
+							<Col xs={1}>
+								<Button onClick={e => actions.removeFoodItem(index, "afternoon")}>
 									<i className="fas fa-minus" />
-								</Col>
-							</Row>
-						</ListGroup.Item>
-					))}
-					<ListGroup.Item>
-						<Diarysearch dailyFood="night" />
+								</Button>
+							</Col>
+						</Row>
 					</ListGroup.Item>
-				</ListGroup>
-			</Container>
-		</>
+				))}
+				<ListGroup.Item>
+					<Diarysearch dailyFood="afternoon" />
+				</ListGroup.Item>
+			</ListGroup>
+
+			<ListGroup className="diary-list-group">
+				<h2>Night</h2>
+				<ListGroup.Item>
+					<Row>
+						<Col>Name</Col>
+						<Col xs={1}>Qty</Col>
+						<Col xs={1}>Serving Size</Col>
+						<Col xs={1}>Unit</Col>
+						<Col xs={1}>Calories</Col>
+						<Col xs={1}>Action</Col>
+					</Row>
+				</ListGroup.Item>
+				{// find the diary for the selected date
+				// check if length > 0 for this part of the day
+				// if length > 0, map the array and display info
+
+				store.foodselected.night.map((item, index) => (
+					<ListGroup.Item key={index}>
+						<Row>
+							<Col>{item.food_name}</Col>
+							<Col xs={1}>
+								<input
+									type="number"
+									min="1"
+									step="1"
+									value={item.qty}
+									onChange={e => actions.updateFoodQty(e.target.value, index, "night")}
+									className="form-control"
+								/>
+							</Col>
+							<Col xs={1}>{item.serving_qty}</Col>
+							<Col xs={1}>{item.serving_unit}</Col>
+							<Col xs={1}>{item.nf_calories}</Col>
+
+							<Col xs={1}>
+								<Button onClick={e => actions.removeFoodItem(index, "night")}>
+									<i className="fas fa-minus" />
+								</Button>
+							</Col>
+						</Row>
+					</ListGroup.Item>
+				))}
+				<ListGroup.Item>
+					<Diarysearch dailyFood="night" />
+				</ListGroup.Item>
+			</ListGroup>
+
+			<ListGroup className="diary-list-group">
+				<h2>Daily Calories</h2>
+				<ListGroup.Item>
+					<Row>
+						<Col>Daily Calories:</Col>
+					</Row>
+				</ListGroup.Item>
+			</ListGroup>
+		</Container>
+
+		// let example = [1,2,3,4,5]
+		// let i;
+		// let sum = 0;
+
+		// for (i = 0; i <example.length; i++){
+		//     sum += example[i];
+		// }
+
+		// alert(sum);
 	);
 };
